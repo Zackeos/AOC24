b10c3c20a7e77368cacf9702964d420f34e154eb
Zackeos <zackfsummer@gmail.com>
2024-12-02 16:15:25 +0000

    Update day2.py

--- day2.py
+++ day2.py
@@ -7,8 +7,8 @@ data = [[int(i) for i in line.split()] for line in data]
 
 
 # Part 1
-def is_safe(report):
-    differences = [report[i + 1] - report[i] for i in range(len(report) - 1)]
+def is_safe(line):
+    differences = [line[i + 1] - line[i] for i in range(len(line) - 1)]
 
     increasing = all(1 <= diff <= 3 for diff in differences)
     decreasing = all(-3 <= diff <= -1 for diff in differences)
@@ -26,12 +26,12 @@ print("Part 1:", part1())
 
 # Part 2
 
-def is_safe_special(report):
-    if is_safe(report):
+def is_safe_special(line):
+    if is_safe(line):
         return True
-    for i in range(len(report)):
-        changed_report = report[:i] + report[i+1:]
-        if is_safe(changed_report):
+    for i in range(len(line)):
+        changed_line = line[:i] + line[i+1:]
+        if is_safe(changed_line):
             return True
 
     return False
@@ -42,10 +42,17 @@ def part2():
         if is_safe_special(item):
             total += 1
     return total
-    
-    
 
 print("Part 2:", part2())
+
+# better solution p2
+
+# :(
+
+
+
+print("Part 2 better:", part2_better())
+
 # submit(part2(), part="b", day=day, year=year)
 
 
