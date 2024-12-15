f9718fce59599110f8ca5ad3a86fd33420a24ee2
Zackeos <zackfsummer@gmail.com>
2024-12-15 20:21:49 +0000

    Update day13.py

--- day13.py
+++ day13.py
@@ -3,12 +3,22 @@ from aocd import submit
 day = 13
 year = 2024
 data = get_data(day=day, year=year).split('\n')
-print(data[:10])
+# print(data[:10])
 
 # Part 1
 
-def part1():
-    pass
+import re
+
+
+def part1(puzzle_input):
+    total = 0
+    tolerance = 0.0001
+    for machine in puzzle_input.split('\n\n'):
+        ax, ay, bx, by, x, y = map(int, re.findall(r'(\d+)', machine))
+        A = (bx*y - by*x) / (bx*ay - by*ax)
+        B = (x-ax*A) / bx
+        if abs(A - round(A)) < tolerance and abs(B - round(B)) < tolerance:
+            total += 3*A + B
 
 
 print("Part 1:", part1())
